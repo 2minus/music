fc1254daf2fe98ef35f07803f1cd30cf08734370
2minus <blecker0610@gmail.com>
2024-06-17 00:04:10 +0900

    AOP 메서드 작동 로그 추가

---
+++ src/main/java/com/prac/music/aop/LogbackAop.java
@@ -0,0 +1,46 @@
+package com.prac.music.aop;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j(topic = "LogbackAop")
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class LogbackAop {
+
+    @Pointcut("execution(* com.prac.music.domain.board.controller.*(..))")
+    private void board() {}
+
+    @Pointcut("execution(* com.prac.music.domain.comment.controller.*(..))")
+    private void comment() {}
+
+    @Pointcut("execution(* com.prac.music.domain.like.controller.*(..))")
+    private void like() {}
+
+    @Pointcut("execution(* com.prac.music.domain.mail.controller.*(..))")
+    private void mail() {}
+
+    @Pointcut("execution(* com.prac.music.domain.user.controller.*(..))")
+    private void user() {}
+
+    @Around("board() && comment() && like() && mail() && user()" )
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info(joinPoint.getSignature().toShortString() + " start");
+
+        try {
+
+            return joinPoint.proceed();
+
+        } finally {
+
+            log.info(joinPoint.getSignature().toShortString() + " end");
+
+        }
+    }
+}
